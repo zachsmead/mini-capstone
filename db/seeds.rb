6f0ca4cf64ecf6ee039c8dbe885e909a9a995d07
= <=>
2017-03-16 21:07:21 -0700

    Added seed dump

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.create!([
+  {name: "Apple", price: "1.95", image: "https://upload.wikimedia.org/wikipedia/commons/1/15/Red_Apple.jpg", description: "A sweet red fruit", in_stock: true},
+  {name: "Orange", price: "2.05", image: "https://upload.wikimedia.org/wikipedia/commons/7/7b/Orange-Whole-%26-Split.jpg", description: "An orange fruit that varies in sourness and sweetness.", in_stock: true},
+  {name: "Banana", price: "1.05", image: "https://upload.wikimedia.org/wikipedia/commons/8/8a/Banana-Single.jpg", description: "A yellow fruit, often used for measuring the size of other objects.", in_stock: true}
+])
